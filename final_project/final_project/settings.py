97b845b8e9d4d345a3d2561be49ad4bdfad74493
Bradley <bradleyballen162004@gmail.com>
2025-09-17 11:36:34 -0500

    was added config to dataBase-postgres

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django,db.backends.postgresql',
+        'NAME': 'final_project_db',
+        'USER': 'postgres',
+        'PASSWORD': '789456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

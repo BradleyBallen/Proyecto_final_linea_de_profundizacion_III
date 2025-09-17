5053ccdf3e90d4953712f9930f1f8304a65cc932
Bradley <bradleyballen162004@gmail.com>
2025-09-17 12:11:32 -0500

    was modified app and DRF

--- mylang/mylang/urls.py
+++ mylang/mylang/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for final_project project.
+URL configuration for mylang project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/

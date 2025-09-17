9b9a7573baa468c2321544748105dd108266f06f
Bradley <bradleyballen162004@gmail.com>
2025-09-17 12:14:56 -0500

    was added models in admin

--- mylang/learning/admin.py
+++ mylang/learning/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Level, Lesson, UserProfile, LevelMembership, Conversation, Message, Progress
 
-# Register your models here.
+admin.site.register(Level)
+admin.site.register(Lesson)
+admin.site.register(UserProfile)
+admin.site.register(LevelMembership)
+admin.site.register(Conversation)
+admin.site.register(Message)
+admin.site.register(Progress)
